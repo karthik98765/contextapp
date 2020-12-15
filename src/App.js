5e1d688d64cae61830a308d20ad2a1f53f2bf57b
karthi <karthi@exm.com>
2020-12-15 22:22:21 +0530

    theme context

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import Booklist from './components/Booklist'
+import Booklist from './components/Booklist';
+import ThemeContextProvider from './context/ThemeContext';
 
 function App() {
   return (
     <div className="App">
+      <ThemeContextProvider>
       <Navbar />
       <Booklist />
+      </ThemeContextProvider>
     </div>
   );
 }
