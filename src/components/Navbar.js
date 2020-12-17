1942335c485f84bfce878ca2a8fe2be259c40d93
karthi <karthi@exm.com>
2020-12-17 14:03:25 +0530

    access context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../context/ThemeContext';
 
 class Navbar extends Component {
-    render() {
+    render() { 
         return (
-            <nav>
-                <h1>Context App</h1>
-                <ul>
+            <ThemeContext.Consumer>{(context) => {
+                const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+                return(
+                <nav style={{ background: theme.ui, color: theme.syntax }}>
+                    <h1>Context App</h1>
+                    <ul>
                     <li>Home</li>
                     <li>About</li>
                     <li>Contact</li>
-                </ul>
-            </nav>
+                    </ul>
+                </nav> 
+                )
+            }}</ThemeContext.Consumer>
         );
     }
 }
