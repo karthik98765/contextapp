26a51d333c8f2e15175fbfdccf84caf834f09616
karthi <karthi@exm.com>
2020-12-21 12:23:17 +0530

    creating context with functions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Booklist from './components/Booklist';
 import ThemeContextProvider from './context/ThemeContext';
 import ThemeToggle from './components/ThemeToggle';
 import AuthContextProvider from './context/AuthContext';
+import BookContextProvider from './context/BookContext';
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
       <ThemeContextProvider>
       <AuthContextProvider>
       <Navbar />
+      <BookContextProvider>
       <Booklist />
+      </BookContextProvider>
       <ThemeToggle />
       </AuthContextProvider>
       </ThemeContextProvider>
